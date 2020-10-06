d7ed4b23f72eeaf9eb965cd01a8ec3b868c14314
Nathaniel Starkman <nstarkman@protonmail.com>
2020-10-06 01:11:27 -0400

    Add pytest template
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

---
+++ python/pyests.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""Testing :mod:`PACKAGE`."""
+
+# __all__ = [
+# ]
+
+
+##############################################################################
+# IMPORTS
+
+# import pytest
+
+##############################################################################
+# PARAMETERS
+
+
+##############################################################################
+# TESTS
+##############################################################################
+
+
+class Test_ClassName(object):
+    """Docstring for ClassName."""
+
+    @classmethod
+    def setup_class(cls):
+        """Setup fixtures for testing."""
+        pass
+
+    # /def
+
+    @classmethod
+    def teardown_class(cls):
+        """Tear-down fixtures for testing."""
+        pass
+
+    # /def
+
+    # -------------------------------
+
+    def test_method(self):
+        """Test :class:`PACKAGE.CLASS.METHOD`."""
+        assert False
+
+    # /def
+
+
+# /class
+
+
+# -------------------------------------------------------------------
+
+
+def test_function():
+    """Test :func:`PACKAGE.METHOD`."""
+    pass
+
+
+# /def
+
+
+##############################################################################
+# END
